ba41e7bd963466e5bbab66f5615d397b94a930a1
David Alvarez <david.alvarez@valid.com>
2021-11-17 23:24:14 -0500

    ft: added security spring configuration with JWT

--- products/src/main/java/com/market/app/products/configuration/SecurityConfig.java
+++ products/src/main/java/com/market/app/products/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -13,9 +14,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
+                .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/auth/**").permitAll();
+                .anyRequest().authenticated();
     }
-
 }
 
